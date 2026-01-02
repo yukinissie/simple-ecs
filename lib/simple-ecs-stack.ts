2797651aae8dc87ba6deb4e8c90a28914ec67908
yukinissie <bigtree2490@outlook.jp>
2026-01-02 12:08:10 +0900

    feat: add initial CDK stack configuration with VPC and Fargate service

--- lib/simple-ecs-stack.ts
+++ lib/simple-ecs-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
 export class SimpleEcsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "SimpleEcsVpc", { maxAzs: 2 });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleEcsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, "SimpleEcsCluster", {
+      vpc: vpc,
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "SimpleFargateService",
+      {
+        cluster: cluster,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+        },
+        desiredCount: 2,
+        publicLoadBalancer: true,
+      }
+    );
   }
 }
